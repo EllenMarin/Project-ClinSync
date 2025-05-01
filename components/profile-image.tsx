3fa4e2c1241fdceed43e8d1445613711a0b5bbe9
EllenMarin <82092612+EllenMarin@users.noreply.github.com>
2025-05-01 21:15:45 +0100

    create profile image component

---
+++ components/profile-image.tsx
@@ -0,0 +1,21 @@
+import { useUser } from "@clerk/nextjs";
+
+export default function ProfileImage(){
+    const {user} = useUser();
+
+    if(!user) return null;
+
+    return (
+        <div className="p-4 gap-4 flex flex-col items-center ">
+          <img
+            src={user.imageUrl}
+            alt="User Profile"
+            className="w-[100px] h-[100px] rounded-full object-cover"
+          />
+          <div className='items-center flex flex-col'>
+            <p className="text-sm font-medium">{user.firstName}</p>
+            <p className="text-xs text-muted-foreground">{user.emailAddresses[0].emailAddress}</p>
+          </div>
+        </div>  
+    )
+}
